edb56cc83e05f077789225ae0952454913a4e7a1
Gabor Szekeres <gabor.szekeres@rabit.hu>
2016-02-17 19:14:45 +0100

    Add LMAdmin - TC1

---
+++ test/lmadmin/TC1.js
@@ -0,0 +1,38 @@
+var common = require("../common");
+var config = common.config;
+var browser = common.browser;
+
+var url = common.bpmPortalUrl;
+
+it("Create LMAdmin instance", function () {
+
+    var xPath = '//table[@role="presentation"]//tr/td/div[normalize-space(text())="LMAdmin"]' +
+        '/ancestor::tr/td/div[normalize-space(text())="Specify Letter Details"]';
+
+    return browser
+        // Load login page
+        .get(url)
+
+        // Log in as user 'ebms'
+        .elementByCss('form[name=loginForm] input[name=BizPassUserID]').type(config.get("ebms.username"))
+        .elementByCss('form[name=loginForm] input[name=BizPassUserPassword]').type(config.get("ebms.password"))
+        .elementByCss('form[name=loginForm] input[type=submit]').click()
+
+        // Click on Applications tab
+        .waitForElementByLinkText('Applications', 10000).click()
+
+        // Click on LMAdmin in Application Name column
+        .waitForElementByLinkText('LMAdmin').click()
+
+        // Click on Create button
+        .frame('AppShowFrame')
+        .waitForElementByCss('a#bizsite_completeTask_CREATE_LABEL_id').click()
+        .frame('TaskShowFrame')
+        .waitForElementById('appNamesCombo')
+
+        // Click on My Worksteps tab and verify
+        .frame()
+        .elementByLinkText('My Worksteps').click()
+        .waitForElementByXPath(xPath);
+
+});
