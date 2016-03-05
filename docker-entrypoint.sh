d4d7308ff276519a1bb9d45d3b24391e8aaa894c
Gabor Szekeres <gabor.szekeres@rabit.hu>
2016-03-05 15:45:07 +0100

    Fix config file path in Docker container startup script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,10 +23,10 @@ if [ -z "$DCM_URL" ]; then
 fi
 
 # patching config
-sed -i "s#\"remote\":.*#\"remote\": \"${SELENIUM_URL}\",#g" test/lib/config.json
-sed -i "s#\"server\.hostname\":.*#\"server\.hostname\": \"${IPM_HOST}\",#g" test/lib/config.json
-sed -i "s#\"ipm\.port\":.*#\"ipm\.port\": \"${IPM_PORT}\",#g" test/lib/config.json
-sed -i "s#\"dcm\.url\":.*#\"dcm\.url\": \"${DCM_URL}\",#g" test/lib/config.json
+sed -i "s#\"remote\":.*#\"remote\": \"${SELENIUM_URL}\",#g" test/config.json
+sed -i "s#\"server\.hostname\":.*#\"server\.hostname\": \"${IPM_HOST}\",#g" test/config.json
+sed -i "s#\"ipm\.port\":.*#\"ipm\.port\": \"${IPM_PORT}\",#g" test/config.json
+sed -i "s#\"dcm\.url\":.*#\"dcm\.url\": \"${DCM_URL}\",#g" test/config.json
 
 echo "USING SELENIUM: ${SELENIUM_URL}"
 echo "USING IPM: ${IPM_HOST}:${IPM_PORT}"
